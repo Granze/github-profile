0c05a0d4f6b41588d79aa17a1941e665ed6f5939
Maurizio Mangione <granzebru@gmail.com>
2014-08-25 23:01:14 +0200

    GH data fetch

--- scripts/components/github-profile.jsx
+++ scripts/components/github-profile.jsx
@@ -3,16 +3,42 @@
 'use strict';
 
 var React = require('react'),
+    xhr = require('superagent'),
 
-  GithubProfile = React.createClass({
-    getInitialState: function () {
-      return {user: 'granze'};
-    },
-    render: function () {
-      return (
-        <div className="container">{this.state.user}</div>
-      );
-    }
-  });
+    GithubProfile = React.createClass({
+      getInitialState: function() {
+        return {user: 'granze'};
+      },
+      componentDidMount: function() {
+        var url = 'https://api.github.com/users/';
+        xhr
+          .get(url + this.state.user)
+          .end(function(res) {
+            console.log(res.body);
+            if (this.isMounted()) {
+              this.setState({
+                name: res.body.name,
+                user : res.body.login,
+                avatar: res.body.avatar_url,
+                url: res.body.html_url,
+                repos: res.body.public_repos,
+                gists: res.body.public_gists
+              });
+            }
+          }.bind(this));
+      },
+      render: function() {
+        return (
+          <div className="github-profile">
+            <h1><a href={this.state.url}><span className="name">{this.state.name}</span>({this.state.user})</a></h1>
+            <img className="avatar" src={this.state.avatar} />
+            <ul className="infos">
+              <li><strong>Repos:</strong> {this.state.repos}</li>
+              <li><strong>Gists:</strong> {this.state.gists}</li>
+            </ul>
+          </div>
+        );
+      }
+    });
 
 module.exports = GithubProfile;
